2c35fc8c7d1c222f62afeffe35545dc7d7cf8d4d
Claudiu <mhvsl@yahoo.com>
2021-03-07 20:19:02 +0100

    2021
    Signed-off-by: Claudiu <mhvsl@yahoo.com>

--- app/src/main/java/com/comp/myth/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/comp/myth/notekeeper/NoteListActivity.java
@@ -2,12 +2,14 @@ package com.comp.myth.notekeeper;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
